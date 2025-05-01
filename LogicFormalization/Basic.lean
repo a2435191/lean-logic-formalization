ec51cac6ef1e9327da75af16d32185dad7c99321
Will Bradley <wjb247@cornell.edu>
2025-05-01 03:16:33 -0400

    chore: update `Basic.lean`

--- LogicFormalization/Basic.lean
+++ LogicFormalization/Basic.lean
@@ -1,10 +1,19 @@
 import LogicFormalization.Chapter1.Section2
+
 import LogicFormalization.Chapter2.Section1.Admissibility
+import LogicFormalization.Chapter2.Section1.Arity
 import LogicFormalization.Chapter2.Section1.DecidableReading
+import LogicFormalization.Chapter2.Section1.Homework
+import LogicFormalization.Chapter2.Section1.NormalForms
 import LogicFormalization.Chapter2.Section1.Notation
 import LogicFormalization.Chapter2.Section1.Prop'
 import LogicFormalization.Chapter2.Section1.Prop'Lemmas
 import LogicFormalization.Chapter2.Section1.Tautologies
+
 import LogicFormalization.Chapter2.Section2.Basic
-import LogicFormalization.Chapter2.Section2.Notation
+import LogicFormalization.Chapter2.Section2.Coloring
 import LogicFormalization.Chapter2.Section2.Completeness
+import LogicFormalization.Chapter2.Section2.Homework
+import LogicFormalization.Chapter2.Section2.Notation
+
+import LogicFormalization.Chapter2.Section3.Basic
